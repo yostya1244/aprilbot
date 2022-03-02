2a33159ce8df3a6c9bd4d32c7d01535b6f116ac6
rizzbot <100199949+rizzbot@users.noreply.github.com>
2022-03-02 21:52:01 +0700

    Update sewa.js

--- shop/sewa.js
+++ shop/sewa.js
@@ -3,12 +3,10 @@ return `「 *PRICE LIST* 」
 							
 ⬣ PRICE SEWABOT
 
+⬡ 1 HARI = Rp 2.000
 ⬡ 1 MINGGU = Rp 5.000
-⬡ 2 MINGGU = Rp 8.000
-⬡ 3 MINGGU = Rp 10.000
-⬡ 1 BULAN = Rp 15.000
-⬡ PERMANEN = Rp 100.000
-⬡ PERMANEN + USER PREM = Rp 150.000
+⬡ 1 BULAN = Rp 18.000
+⬡ PERMANEN = Rp 10.000
 
 
 ⬣ PRICE JADIBOT
@@ -17,12 +15,12 @@ return `「 *PRICE LIST* 」
 
 
 ⬣ MINAT? PM
-⬡ wa.me/62887435047326
+⬡ wa.me/6285707708715
 
 
 NOTE : 
 Bot On 24 Jam Karna Di Run Menggunakan Heroku
-Reset Data Setiap Jam 02:00 `
+Reset Data Setiap Jam 07:00 `
 	}
 
 exports.pc_sewa = pc_sewa
